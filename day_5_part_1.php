a574cc51a3b0afd847a1c64124ab55b149b8ca50
Martin Sandberg <martin.sandberg@evry.com>
2020-12-05 14:21:06 +0100

    Cleanp day 5

--- day_5_part_1.php
+++ day_5_part_1.php
@@ -3,20 +3,17 @@
 $boardingPasses = include 'day_5_input.php';
 
 $maxSeatId = 0;
+
 foreach ($boardingPasses as $boardingPass) {
     $row = 0;
-    $max = 127;
     for ($i = 0; $i < 7; $i++) {
-        $max = $max - (pow(2, (6-$i)) * ($boardingPass[$i] == 'F' ? 1 : 0));
-        $row = $row + (pow(2, (6-$i)) * ($boardingPass[$i] == 'B' ? 1 : 0));
+        $row = $row + pow(2, (6-$i)) * ($boardingPass[$i] == 'B' ? 1 : 0);
     }
     $col = 0;
-    $max = 7;
     for ($i = 0; $i < 3; $i++) {
-        $max = $max - (pow(2, (2-$i)) * ($boardingPass[$i+7] == 'L' ? 1 : 0));
-        $col = $col + (pow(2, (2-$i)) * ($boardingPass[$i+7] == 'R' ? 1 : 0));
+        $col = $col + pow(2, (2-$i)) * ($boardingPass[$i+7] == 'R' ? 1 : 0);
     }
-    $seatId = ($row * 8) + $col;
+    $seatId = $row * 8 + $col;
     $maxSeatId = $seatId > $maxSeatId ? $seatId : $maxSeatId;
 }
 
--- day_5_part_2.php
+++ day_5_part_2.php
@@ -4,23 +4,17 @@ $boardingPasses = include 'day_5_input.php';
 
 $takenSeats = [];
 
-$maxSeatId = 0;
 foreach ($boardingPasses as $boardingPass) {
     $row = 0;
-    $max = 127;
-    for ($i = 0; $i < 7; $i++) {
-        $max = $max - (pow(2, (6 - $i)) * ($boardingPass[$i] == 'F' ? 1 : 0));
-        $row = $row + (pow(2, (6 - $i)) * ($boardingPass[$i] == 'B' ? 1 : 0));
-    }
     $col = 0;
-    $max = 7;
-    for ($i = 0; $i < 3; $i++) {
-        $max = $max - (pow(2, (2 - $i)) * ($boardingPass[$i + 7] == 'L' ? 1 : 0));
-        $col = $col + (pow(2, (2 - $i)) * ($boardingPass[$i + 7] == 'R' ? 1 : 0));
+    for ($i = 0; $i < 7; $i++) {
+        $row = $row + pow(2, (6 - $i)) * ($boardingPass[$i] == 'B' ? 1 : 0);
     }
-    $seatId = ($row * 8) + $col;
+    for ($i = 0; $i < 3; $i++) {
+        $col = $col + pow(2, (2 - $i)) * ($boardingPass[$i + 7] == 'R' ? 1 : 0);
+    }
+    $seatId = $row * 8 + $col;
     $takenSeats[] = $seatId;
-    $maxSeatId = $seatId > $maxSeatId ? $seatId : $maxSeatId;
 }
 
 for ($i = 8; $i < 126 * 8; $i++) {
@@ -29,6 +23,3 @@ for ($i = 8; $i < 126 * 8; $i++) {
         break;
     }
 }
-
-
-
