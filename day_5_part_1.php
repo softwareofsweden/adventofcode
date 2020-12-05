cd533c0cfd116b204364b9c1df92cf4b36e9b322
Martin Sandberg <martin.sandberg@evry.com>
2020-12-05 14:35:22 +0100

    Cleanp day 5

--- day_5_part_1.php
+++ day_5_part_1.php
@@ -6,15 +6,12 @@ $maxSeatId = 0;
 
 foreach ($boardingPasses as $boardingPass) {
     $row = 0;
-    for ($i = 0; $i < 7; $i++) {
-        $row = $row + pow(2, (6-$i)) * ($boardingPass[$i] == 'B' ? 1 : 0);
-    }
     $col = 0;
-    for ($i = 0; $i < 3; $i++) {
-        $col = $col + pow(2, (2-$i)) * ($boardingPass[$i+7] == 'R' ? 1 : 0);
+    for ($i = 0; $i < 10; $i++) {
+        $row = $row + ($i < 7 ? (pow(2, (6 - $i)) * ($boardingPass[$i] == 'B' ? 1 : 0)) : 0);
+        $col = $col + ($i > 6 ? (pow(2, (9 - $i)) * ($boardingPass[$i] == 'R' ? 1 : 0)) : 0);
     }
-    $seatId = $row * 8 + $col;
-    $maxSeatId = $seatId > $maxSeatId ? $seatId : $maxSeatId;
+    $maxSeatId = $row * 8 + $col > $maxSeatId ? $row * 8 + $col : $maxSeatId;
 }
 
 echo 'The answer is: ' . $maxSeatId . "\n";
--- day_5_part_2.php
+++ day_5_part_2.php
@@ -7,14 +7,11 @@ $takenSeats = [];
 foreach ($boardingPasses as $boardingPass) {
     $row = 0;
     $col = 0;
-    for ($i = 0; $i < 7; $i++) {
-        $row = $row + pow(2, (6 - $i)) * ($boardingPass[$i] == 'B' ? 1 : 0);
+    for ($i = 0; $i < 10; $i++) {
+        $row = $row + ($i < 7 ? (pow(2, (6 - $i)) * ($boardingPass[$i] == 'B' ? 1 : 0)) : 0);
+        $col = $col + ($i > 6 ? (pow(2, (9 - $i)) * ($boardingPass[$i] == 'R' ? 1 : 0)) : 0);
     }
-    for ($i = 0; $i < 3; $i++) {
-        $col = $col + pow(2, (2 - $i)) * ($boardingPass[$i + 7] == 'R' ? 1 : 0);
-    }
-    $seatId = $row * 8 + $col;
-    $takenSeats[] = $seatId;
+    $takenSeats[] = $row * 8 + $col;
 }
 
 for ($i = 8; $i < 126 * 8; $i++) {
