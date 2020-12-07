1f5affff70d9bd92ff90b151a8d6195d923b653f
Martin Sandberg <martin.sandberg@evry.com>
2020-12-07 09:14:28 +0100

    Day 7

---
+++ day_7_part_1.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once "ProblemInterface.php";
+
+class Day7Part1 implements ProblemInterface
+{
+
+    public function bagHasBag($allBags, $bag, $find)
+    {
+        if (array_key_exists($find, $bag)) {
+            return true;
+        } else {
+            foreach ($bag as $innerBag => $count) {
+                if ($this->bagHasBag($allBags, $allBags[$innerBag], $find)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function solve()
+    {
+        $input = include 'day_7_input.php';
+        $find = 'shiny gold bag';
+        $totalCount = 0;
+        $bags = [];
+        for ($i = 0; $i < count($input); $i++) {
+            $parts = explode('contain ', $input[$i]);
+            $parts[0] = trim(str_replace('bags', 'bag', $parts[0]));
+            $bags[$parts[0]] = [];
+            $content = explode(', ', $parts[1]);
+            foreach ($content as $innerBag) {
+                $innerBag = trim(str_replace('.', '', $innerBag));
+                $count = substr($innerBag, 0, strpos($innerBag, ' '));
+                $innerBag = str_replace('bags', 'bag', substr($innerBag, strpos($innerBag, ' ') + 1));
+                if ($innerBag != 'other bag') {
+                    $bags[$parts[0]][$innerBag] = $count;
+                }
+            }
+        }
+        foreach ($bags as $bag) {
+            if ($this->bagHasBag($bags, $bag, $find)) {
+                $totalCount++;
+            };
+        }
+        return $totalCount;
+    }
+}
---
+++ day_7_part_2.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once "ProblemInterface.php";
+
+class Day7Part2 implements ProblemInterface
+{
+
+    public function bagCount($allBags, $bag) {
+        $count = 0;
+        foreach ($bag as $innerBag => $c) {
+            $count = $count + $c;
+            $count += $this->bagCount($allBags, $allBags[$innerBag]) * $c;
+        }
+        return $count;
+    }
+
+    public function solve()
+    {
+        $input = include 'day_7_input.php';
+        $find = 'shiny gold bag';
+        $bags = [];
+        for ($i = 0; $i < count($input); $i++) {
+            $parts = explode('contain ', $input[$i]);
+            $parts[0] = trim(str_replace('bags', 'bag', $parts[0]));
+            $bags[$parts[0]] = [];
+            $content = explode(', ', $parts[1]);
+            foreach ($content as $innerBag) {
+                $innerBag = trim(str_replace('.', '', $innerBag));
+                $count = substr($innerBag, 0, strpos($innerBag, ' '));
+                $innerBag = str_replace('bags', 'bag', substr($innerBag, strpos($innerBag, ' ') + 1));
+                if ($innerBag != 'other bag') {
+                    $bags[$parts[0]][$innerBag] = $count;
+                }
+            }
+        }
+        $totalCount = $this->bagCount($bags, $bags[$find]);
+        return $totalCount;
+    }
+}
