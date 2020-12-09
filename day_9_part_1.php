a71cec897a6e0818ee0e489a295e2f07f0b58e17
Martin Sandberg <martin.sandberg@evry.com>
2020-12-09 08:36:30 +0100

    Day 9

---
+++ day_9_part_1.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "ProblemInterface.php";
+
+class Day9Part1 implements ProblemInterface
+{
+    public function expected()
+    {
+        return 15690279;
+    }
+
+    public function solve()
+    {
+        $numbers = include 'day_9_input.php';
+        for ($i = 25; $i < count($numbers); $i++)
+        {
+            $isSum = false;
+            for ($j = $i - 25; $j < $i + 25; $j++) {
+                for ($k = $j + 1; $k < $i + 25; $k++) {
+                    if ($numbers[$j] + $numbers[$k] == $numbers[$i]) {
+                        $isSum = true;
+                        break 2;
+                    }
+                }
+            }
+            if (!$isSum) {
+                return $numbers[$i];
+            }
+        }
+        return -1;
+    }
+}
---
+++ day_9_part_2.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "ProblemInterface.php";
+
+class Day9Part2 implements ProblemInterface
+{
+    public function expected()
+    {
+        return 2174232;
+    }
+
+    public function solve()
+    {
+        $numbers = include 'day_9_input.php';
+        $numberToFind = 15690279;
+        for ($offset = 0; $offset < count($numbers); $offset++)
+        {
+            $maxLength = count($numbers) - $offset;
+            for ($length = 2; $maxLength; $length++) {
+                $slice = array_slice($numbers, $offset, $length);
+                $sum = array_sum($slice);
+                if ($sum == $numberToFind) {
+                    return min($slice) + max($slice);
+                }
+                if ($sum > $numberToFind) {
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+}
